880de7714c796672643c36605e4ba4c3a99b365b
wanyu790103 <teddy790103@gmail.com>
2025-05-28 12:50:21 +0800

    feat_聖誕樹_JavaScript

---
+++ js_1.js
@@ -0,0 +1,24 @@
+function Tree(leafSymbol = '*', decoSymbol = 'o', height = 5) {
+
+  for (let i = 0; i < height; i++) {
+    let spaces = ' '.repeat(height - i - 1);
+    let row = '';
+
+    for (let j = 0; j <= i; j++) {
+      row += leafSymbol;
+      if (j !== i) {
+        row += decoSymbol;
+      }
+    }
+
+    console.log(spaces + row);
+  }
+
+  const trunkWidth = 3;
+  const trunk = '|'.repeat(trunkWidth);
+  const trunkSpaces = ' '.repeat(height - Math.floor(trunkWidth / 2) - 1);
+  console.log(trunkSpaces + trunk);
+}
+
+
+Tree('*', '0', 5);
